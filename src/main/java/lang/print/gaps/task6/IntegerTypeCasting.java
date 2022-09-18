5aaafc6ebd4fc1c4dbf20e971bb3ce17a44fc7b6
Igor Bugaiov <no-reply>
2022-09-18 10:23:24 -0700

    st0-mod3-t6 1st try

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long first = 1000l;
+        long second = 1000l;
+        first = (int) second;
+        System.out.printf("%d\n%d\n", first, second);
     }
 }
